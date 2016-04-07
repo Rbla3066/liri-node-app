a9f6e69ef12b2e171b1c2c62689630259141346f
Robert Black <rbla4655@gmail.com>
2016-04-07 17:09:53 -0400

    finished work

--- liri.js
+++ liri.js
@@ -48,3 +48,36 @@ function getTweets(){
 	  }else{console.log(error)}
 	});
 };
+function getSong(uParam){
+	if(uParam == undefined) uParam = "what's my age again";
+	spotify.search({type: "track", query: uParam}, function(err, data){
+		var song = data.tracks.items[0];
+		console.log("Artist: " + song.artists[0].name)
+		console.log("Song:" + song.name)
+		console.log("Preview: " + song.preview_url)
+		console.log("Album: " + song.album.name)
+	})
+};
+function getMovie(uParam){
+	if(uParam == undefined) uParam = "Mr. Nobody";
+	request('http://www.omdbapi.com/?t='+uParam+'&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var mov = JSON.parse(body);
+			console.log("\nTitle: " + mov.Title);
+			console.log("Year: " + mov.Year);
+			console.log("Country: " + mov.Country);
+			console.log("Language: " + mov.Language);
+			console.log("Actors: " + mov.Actors);
+			console.log("IMDB Rating: " + mov.imdbRating);
+			console.log("Rotten Tomatoes Rating: " + mov.tomatoRating);
+			console.log("Rotten Tomatoe URL: " + mov.tomatoURL);
+		};
+	});
+};
+function doWhatItSays(){
+	fs.readFile("random.txt", "utf-8", function(err, data){
+		if(err) console.log(err);
+		var params = data.split(", ");
+		directUser(params[0], params[1]);
+	});
+};
